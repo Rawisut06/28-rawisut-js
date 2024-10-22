6533b23e028baa2615405b75088fc7043b121dfe
rawisut06 <tungtung0685@gmail.com>
2024-10-22 11:10:55 +0700

    create shopping cart with html/css

---
+++ script.js
@@ -0,0 +1,6 @@
+const productName = document.querySelector(".product-name");
+const productPrice = document.querySelector(".product-price");
+const productImage = document.querySelector(".product-image");
+const createBtn = document.querySelector(".create-product");
+const addBtn = document.querySelector(".add-cart");
+const calcBtn = document.querySelector(".calc-price");
